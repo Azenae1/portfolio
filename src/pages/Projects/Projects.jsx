61ce204777ef99d5b1348f3f7d1d35d3b70dbd11
Yury Bursian <dab180996@gmail.com>
2024-12-11 22:59:12 +0200

    projects list created

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,30 +1,7 @@
 import React from "react";
 import "./Projects.css";
 import Header from "../../components/Header/Header";
-import Image from "../../assets/logo.png";
-
-const projects = [
-  {
-    id: 1,
-    title: "Project One",
-    description:
-      "This is the first project description. It showcases a unique feature.",
-    image: Image,
-  },
-  {
-    id: 2,
-    title: "Project Two",
-    description:
-      "The second project has an innovative design and rich functionality.",
-    image: Image,
-  },
-  {
-    id: 3,
-    title: "Project Three",
-    description: "Project three is focused on performance and scalability.",
-    image: Image,
-  },
-];
+import { projects } from "../../utils/constants";
 
 const Projects = () => {
   return (
---
+++ src/utils/constants.js
@@ -0,0 +1,24 @@
+import Image from "../assets/logo.png";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Project One",
+    description:
+      "This is the first project description. It showcases a unique feature.",
+    image: Image,
+  },
+  {
+    id: 2,
+    title: "Project Two",
+    description:
+      "The second project has an innovative design and rich functionality.",
+    image: Image,
+  },
+  {
+    id: 3,
+    title: "Project Three",
+    description: "Project three is focused on performance and scalability.",
+    image: Image,
+  },
+];
